ba251f0ee5191836cb828b679c399629bbaf1c2a
Kyle Cameron <1kylecameron@gmail.com>
2017-01-26 13:39:05 -0500

    commiting unstaged changes before rebasing.

--- server/controller/profController.js
+++ server/controller/profController.js
@@ -10,40 +10,42 @@ var Users = require('../../app/collections/users');
 var Dogs = require('../../app/collections/dogs');
 var Walkers = require('../../app/collections/walkers');
 
-exports.displayProf = function(req, res) {
-  console.log('getting profile data!');
-  console.log('req.body: ', req.body);
+module.exports = {
+  displayProf: function(req, res) {
+    console.log('getting profile data!');
+    console.log('req.body: ', req.body);
 
-  var username = req.params.username;
-  var profile = {}
-  // search the db for a particular username
-  new User({username: username})
-  .fetch()
-  .then(function(user) {
-    if (!user) {
-      // if !found, do... something?
-      console.log('user not found!');
-      res.redirect('/'); // PLACEHOLDER
-      // res.send();  // should probably send something else
-    } else {
-      // if found: grab the user profile data
-      Object.assign(profile, user.toJSON());
-      var userId = user.get('userId');
-
-      if (user.get('isDog')) {
-        new Dog({userId: userId}).fetch()
-        .then(function(dog) {
-          Object.assign(profile, dog.toJSON());
-        });
+    var username = req.params.username;
+    var profile = {}
+    // search the db for a particular username
+    new User({username: username})
+    .fetch()
+    .then(function(user) {
+      if (!user) {
+        // if !found, do... something?
+        console.log('user not found!');
+        res.redirect('/'); // PLACEHOLDER
+        // res.send();  // should probably send something else
       } else {
-        new Walker({userId: userId}).fetch()
-        .then(function(walker) {
-          Object.assign(profile, walker.toJSON());
-        });
+        // if found: grab the user profile data
+        Object.assign(profile, user.toJSON());
+        var userId = user.get('userId');
+
+        if (user.get('isDog')) {
+          new Dog({userId: userId}).fetch()
+          .then(function(dog) {
+            Object.assign(profile, dog.toJSON());
+          });
+        } else {
+          new Walker({userId: userId}).fetch()
+          .then(function(walker) {
+            Object.assign(profile, walker.toJSON());
+          });
+        }
+        // build a bookshelf query and search the appropriate table
+        // res.redirect('/'); // PLACEHOLDER
+        res.send(profile);
       }
-      // build a bookshelf query and search the appropriate table
-      // res.redirect('/'); // PLACEHOLDER
-      res.send(profile);
-    }
-  });
+    });
+  }
 }
